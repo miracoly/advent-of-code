200d32a98ceae1156b6c79a26bd1b3f6b289c1b1
miracoly <68049792+miracoly@users.noreply.github.com>
2023-12-12 22:35:41 +0100

    feat(23): add challenge 2 of day 1

--- 2023/day-01/day_01.c
+++ 2023/day-01/day_01.c
@@ -11,6 +11,10 @@
 
 static uint8_t get_calibration_of_line(char line[static 1]);
 
+static void update_finding(uint8_t *left, uint8_t *right, uint8_t num);
+
+static uint8_t lookup_number_text(char text[static 1]);
+
 static uint8_t char_to_int(char c);
 
 
@@ -39,18 +43,41 @@ static uint8_t get_calibration_of_line(char line[static 1]) {
     uint8_t left = 0;
     uint8_t right = 0;
     for (size_t i = 0; i < length; ++i) {
-        if (isdigit(line[i]) && !left) {
-            left = char_to_int(line[i]);
-        }
         if (isdigit(line[i])) {
-            right = char_to_int(line[i]);
+            update_finding(&left, &right, char_to_int(line[i]));
+            continue;
+        }
+
+        char potenzial_number[5];
+        strncpy(potenzial_number, line + i, 5);
+        const uint8_t maybe_num = lookup_number_text(potenzial_number);
+        if (UINT8_MAX - maybe_num) {
+            update_finding(&left, &right, maybe_num);
         }
     }
-    return (!left || !right)
-           ? left + right
-           : (left * 10) + right;
+    return (left * 10) + right;
+}
+
+static void update_finding(uint8_t *left, uint8_t *right, uint8_t num) {
+    if (!*left) {
+        *left = num;
+    }
+    *right = num;
 }
 
 static uint8_t char_to_int(char c) {
     return c - '0';
+}
+
+static uint8_t lookup_number_text(char text[static 1]) {
+    if (!strncmp(text, "one", 3)) { return 1; }
+    if (!strncmp(text, "two", 3)) { return 2; }
+    if (!strncmp(text, "three", 5)) { return 3; }
+    if (!strncmp(text, "four", 4)) { return 4; }
+    if (!strncmp(text, "five", 4)) { return 5; }
+    if (!strncmp(text, "six", 3)) { return 6; }
+    if (!strncmp(text, "seven", 5)) { return 7; }
+    if (!strncmp(text, "eight", 5)) { return 8; }
+    if (!strncmp(text, "nine", 4)) { return 9; }
+    else { return UINT8_MAX; }
 }
