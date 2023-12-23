fa829c6ca8d5e3031201018077a8d3e011ca477a
miracoly <68049792+miracoly@users.noreply.github.com>
2023-12-23 21:05:02 +0100

    feat(3-23): add challenge 2 of day 3

--- 2023/day-03/day_03.c
+++ 2023/day-03/day_03.c
@@ -32,7 +32,7 @@ static uint8_t char_to_int(char c) {
 }
 
 static bool is_symbol(char c) {
-    return (c != '.') && !isdigit(c);
+    return (c == '*');
 }
 
 static unsigned int pow10(uint8_t e) {
@@ -53,27 +53,14 @@ static bool is_num_included(
     const bool left = col != 0 && is_symbol(buffer[row][col - 1]);
     const bool top = row != 0 && is_symbol(buffer[row - 1][col]);
     const bool top_right =
-            row != 0
-            && col + 1 < LINE_LENGTH_MAX
-            && is_symbol(buffer[row - 1][col + 1]);
-    const bool top_left =
-            row != 0
-            && col != 0
-            && is_symbol(buffer[row - 1][col - 1]);
-    const bool bottom =
-            row < LINES_MAX - 1
-            && is_symbol(buffer[row + 1][col]);
+            row != 0 && col + 1 < LINE_LENGTH_MAX && is_symbol(buffer[row - 1][col + 1]);
+    const bool top_left = row != 0 && col != 0 && is_symbol(buffer[row - 1][col - 1]);
+    const bool bottom = row < LINES_MAX - 1 && is_symbol(buffer[row + 1][col]);
     const bool bottom_right =
-            row < LINES_MAX - 1
-            && col + 1 < LINE_LENGTH_MAX
-            && is_symbol(buffer[row + 1][col + 1]);
-    const bool bottom_left =
-            row < LINES_MAX - 1
-            && col != 0
-            && is_symbol(buffer[row + 1][col - 1]);
+            row < LINES_MAX - 1 && col + 1 < LINE_LENGTH_MAX && is_symbol(buffer[row + 1][col + 1]);
+    const bool bottom_left = row < LINES_MAX - 1 && col != 0 && is_symbol(buffer[row + 1][col - 1]);
 
-    return right || left || top || top_right || top_left || bottom ||
-           bottom_right || bottom_left;
+    return right || left || top || top_right || top_left || bottom || bottom_right || bottom_left;
 }
 
 static int sum_part_numbers(const char buffer[LINES_MAX][LINE_LENGTH_MAX]) {
@@ -106,6 +93,124 @@ static int sum_part_numbers(const char buffer[LINES_MAX][LINE_LENGTH_MAX]) {
     return total;
 }
 
+typedef struct {
+    bool top_left;
+    bool top;
+    bool top_right;
+    bool left;
+    bool right;
+    bool bottom_left;
+    bool bottom;
+    bool bottom_right;
+} findings;
+
+static unsigned int get_num_to_left(size_t col, const char buf_line[LINE_LENGTH_MAX]) {
+    if (col > LINE_LENGTH_MAX - 1) return 0;
+
+    unsigned int num = 0;
+    for (size_t i = col; i < LINE_LENGTH_MAX; --i) {
+        const uint8_t e = col - i;
+        if (isdigit(buf_line[i])) {
+            num += char_to_int(buf_line[i]) * pow10(e);
+        } else {
+            break;
+        }
+    }
+    return num;
+}
+
+static unsigned int get_num_to_right(size_t col, const char buf_line[LINE_LENGTH_MAX]) {
+    if (col == LINE_LENGTH_MAX - 1) return 0;
+
+    unsigned int num = 0;
+    for (size_t i = col; i < LINE_LENGTH_MAX; ++i) {
+        if (isdigit(buf_line[i])) {
+            num = (num * 10) + char_to_int(buf_line[i]);
+        } else {
+            break;
+        }
+    }
+    return num;
+}
+
+static unsigned int get_product_of_findings(
+        findings f,
+        size_t row,
+        size_t col,
+        const char buffer[LINES_MAX][LINE_LENGTH_MAX]
+) {
+    const unsigned int left = f.left ? get_num_to_left(col - 1, buffer[row]) : 1;
+    const unsigned int right = f.right ? get_num_to_right(col + 1, buffer[row]) : 1;
+    const unsigned int top_left =
+            f.top_left && !f.top ? get_num_to_left(col - 1, buffer[row - 1]) : 1;
+    const unsigned int top_right =
+            f.top_right && !f.top ? get_num_to_right(col + 1, buffer[row - 1]) : 1;
+    const unsigned int bottom_left =
+            f.bottom_left && !f.bottom ? get_num_to_left(col - 1, buffer[row + 1]) : 1;
+    const unsigned int bottom_right =
+            f.bottom_right && !f.bottom ? get_num_to_right(col + 1, buffer[row + 1]) : 1;
+    const unsigned int only_top =
+            f.top && !f.top_left && !f.top_right ? char_to_int(buffer[row - 1][col]) : 1;
+    const unsigned int top_1 =
+            f.top_right && f.top && !f.top_left ? get_num_to_right(col, buffer[row - 1]) : 1;
+    const unsigned int top_2 =
+            f.top_left && f.top && !f.top_right ? get_num_to_left(col, buffer[row - 1]) : 1;
+    const unsigned int top_3 =
+            f.top && f.top_left && f.top_right ? get_num_to_left(col + 1, buffer[row - 1]) : 1;
+    const unsigned int only_bottom =
+            f.bottom && !f.bottom_left && !f.bottom_right ? char_to_int(buffer[row + 1][col]) : 1;
+    const unsigned int bottom_1 =
+            f.bottom_right && f.bottom && !f.bottom_left ? get_num_to_right(col, buffer[row + 1]) : 1;
+    const unsigned int bottom_2 =
+            f.bottom_left && f.bottom && !f.bottom_right ? get_num_to_left(col, buffer[row + 1]) : 1;
+    const unsigned int bottom_3 =
+            f.bottom && f.bottom_left && f.bottom_right ? get_num_to_left(col + 1, buffer[row + 1])
+                                                        : 1;
+
+    return left * right * top_left * top_right * bottom_left * bottom_right * only_top * top_1 *
+           top_2 * top_3 * only_bottom * bottom_1 * bottom_2 * bottom_3;
+}
+
+static unsigned int get_gear_ratio(
+        size_t row,
+        size_t col,
+        const char buffer[LINES_MAX][LINE_LENGTH_MAX]
+) {
+    const findings f = {
+            .top_left = row != 0 && col != 0 && isdigit(buffer[row - 1][col - 1]),
+            .top = row != 0 && isdigit(buffer[row - 1][col]),
+            .top_right = row != 0 && col + 1 < LINE_LENGTH_MAX &&
+                         isdigit(buffer[row - 1][col + 1]),
+            .left =  col != 0 && isdigit(buffer[row][col - 1]),
+            .right = col + 1 < LINE_LENGTH_MAX && isdigit(buffer[row][col + 1]),
+            .bottom_left = row < LINES_MAX - 1 && col != 0 && isdigit(buffer[row + 1][col - 1]),
+            .bottom = row < LINES_MAX - 1 && isdigit(buffer[row + 1][col]),
+            .bottom_right = row < LINES_MAX - 1 && col + 1 < LINE_LENGTH_MAX &&
+                            isdigit(buffer[row + 1][col + 1])
+    };
+
+    const uint8_t count_part_nums =
+            f.left + f.right
+            + (f.top ? 1 : f.top_left + f.top_right)
+            + (f.bottom ? 1 : f.bottom_left + f.bottom_right);
+
+    return count_part_nums == 2 ? get_product_of_findings(f, row, col, buffer) : 0;
+}
+
+static unsigned long sum_gear_ratios(const char buffer[LINES_MAX][LINE_LENGTH_MAX]) {
+    unsigned long total = 0;
+    unsigned long findings = 0;
+    for (size_t row = 0; row < LINES_MAX; ++row) {
+        for (size_t col = 0; col < LINE_LENGTH_MAX; ++col) {
+            if (is_symbol(buffer[row][col])) {
+                total += get_gear_ratio(row, col, buffer);
+                findings++;
+            }
+        }
+    }
+    return total;
+}
+
 int main(void) {
     FILE *file = fopen("./input.txt", "r");
     if (!file) return EXIT_FAILURE;
@@ -113,9 +218,9 @@ int main(void) {
     char buffer[LINES_MAX][LINE_LENGTH_MAX] = {0};
     to_buffer(LINE_LENGTH_MAX, LINES_MAX, file, buffer);
 
-    unsigned int total = sum_part_numbers(buffer);
+    unsigned long total = sum_gear_ratios(buffer);
 
-    printf("Result: %u", total);
+    printf("Result: %lu", total);
 
     fclose(file);
     return EXIT_SUCCESS;
