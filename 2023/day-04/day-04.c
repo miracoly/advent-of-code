92ba3d0f71591981f32f81311191d2c46427c06e
miracoly <68049792+miracoly@users.noreply.github.com>
2024-01-14 15:14:45 +0100

    WIP

--- 2023/day-04/day-04.c
+++ 2023/day-04/day-04.c
@@ -1,9 +1,23 @@
 #include "day-04.h"
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include "../lib/aoc-lib.h"
 
-unsigned int get_points_per_card(const char card[static 1]) {
-    printf("%s", card);
-    return 1;
+unsigned int get_points_per_card(char card[static 1]) {
+    char* split1[strlen(card)];
+    const size_t len_split1 = aoc_split(strlen(card), card, split1, ":");
+    assert(len_split1 == 2);
+
+    char* split2[len_split1];
+    const size_t len_split2 = aoc_split(len_split1, split1[1], split2, "|");
+    assert(len_split2 == 2);
+
+    const size_t len_winning_nums = aoc_reduce_spaces(strlen(split2[0]), split2[0]);
+//    size_t len_after_split = aoc_split(len_after_clean, card, split_str, " ");
+
+    printf("%s\n", split2[0]);
+    return len_winning_nums;
 }
 
 unsigned int challenge_1(FILE* file) {
--- 2023/day-04/day-04.h
+++ 2023/day-04/day-04.h
@@ -6,6 +6,6 @@
 #define LINE_LENGTH_MAX 128
 
 unsigned int challenge_1(FILE *file);
-unsigned int get_points_per_card(const char card[static 1]);
+unsigned int get_points_per_card(char card[static 1]);
 
 #endif //ADVENT_OF_CODE_DAY_04_H
